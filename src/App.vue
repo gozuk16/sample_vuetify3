f78caf57e8c83371322d5263f4e915bc3d74f8e6
GOZU Kenichiro <gozuk16@gmail.com>
2022-05-14 15:42:11 +0900

    cardを並べてみる

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+   <v-app>
+      <v-app-bar color="grey-lighten-2">Appバー</v-app-bar>
+      <v-main>
+        <Sample />
+      </v-main>
+    </v-app>
+
+</template>
+
+<script setup>
+import Sample from './components/Sample.vue'
+</script>
---
+++ src/components/Sample.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="4">
+        <v-card height="300px" class="mb-3">カード1</v-card>
+        <v-card height="300px">カード2</v-card>
+      </v-col>
+      <v-col>
+        <v-card height="400px">カード3</v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
